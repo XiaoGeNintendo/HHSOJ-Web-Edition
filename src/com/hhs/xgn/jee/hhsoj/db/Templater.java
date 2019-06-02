cd39ae9333bd417ff758939b786b5c7f1ee22811
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2019-06-02 14:10:20 +0800

    Update Templater.java

--- src/com/hhs/xgn/jee/hhsoj/db/Templater.java
+++ src/com/hhs/xgn/jee/hhsoj/db/Templater.java
@@ -4,7 +4,7 @@ import com.hhs.xgn.jee.hhsoj.type.Problem;
 
 public class Templater {
 	public static String loadTemplate(String s,Problem p){
-		String st="<h2>Edit "+s+"</h2><textarea id=\"edit_"+s+"\" rows=20 cols=100>"+FileHelper.readFileFull(p.getPath()+"/"+s)+"</textarea><button onclick=\"update("+s+")\">Update File</button>";
+		String st="<h2>Edit "+s+"</h2><textarea id=\"edit_"+s+"\" rows=20 cols=100>"+FileHelper.readFileFull(p.getPath()+"/"+s)+"</textarea><button onclick=\"update('"+s+"')\">Update File</button>";
 		return st;
 	}
 }
